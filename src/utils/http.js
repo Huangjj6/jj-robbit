1bb96c3f0c0957335c3f3a378b5ef59cda1a095d
HJJ <1011548020@qq.com>
2025-07-10 12:45:27 +0800

    401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,7 @@
 //axios基础封装
 import axios from "axios";
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 //创建axios实例
 const httpInstance = axios.create({
@@ -21,11 +22,18 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore=useUserStore()
   //统一错误提示
   ElMessage({
     type:'warning',
     message:e.response.data.msg
   })
+  //401错误，token失效处理
+  if(e.response.status===401){
+    
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 

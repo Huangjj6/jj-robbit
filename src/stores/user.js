6e059cf727e47b3dd41a02390b735a9603f654df
HJJ <1011548020@qq.com>
2025-07-10 11:28:17 +0800

    退出登录业务实现

--- src/stores/user.js
+++ src/stores/user.js
@@ -8,7 +8,10 @@ export const useUserStore=defineStore('user',()=>{
         const res=await loginAPI({account,password})
         userInfo.value=res.result
     }
-    return{userInfo,getUserInfo}
+    const clearUserInfo=()=>{
+        userInfo.value={}
+    }
+    return{userInfo,getUserInfo,clearUserInfo}
 },
 {
     persist:true

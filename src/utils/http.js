95813a8d6fa9cbbf4ecd8cd89560f64b5c1f9850
HJJ <1011548020@qq.com>
2025-07-12 21:42:45 +0800

    头部购物车列表渲染

--- src/utils/http.js
+++ src/utils/http.js
@@ -6,7 +6,7 @@ import {ElMessage} from 'element-plus'
 
 //创建axios实例
 const httpInstance = axios.create({
-    baseURL:'https://pcapi-xiaotuxian-front-devtest.itheima.net/',
+    baseURL:'https://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout:5000,
 })
 
